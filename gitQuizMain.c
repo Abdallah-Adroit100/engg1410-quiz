2b563222a165c10c16e5044cc431fe829204fb66
Abdallah Al Hussami <alhussaa@uoguelph.ca>
2022-10-31 10:19:29 -0400

    added gitQuizMain.c file

--- gitQuizMain.c
+++ gitQuizMain.c
@@ -7,13 +7,7 @@ int main(void)
     printf("Enter 'y' and press enter if you know how to use Git!\n");
     scanf(" %c", &val);
 
-    if(val == 'y'){
-        printf("You know how to use Git!");
-    }eles{
-        printf("I don't have git account")
-    }
-
-    printf("\n");
+    
 
     return 0;
 }
